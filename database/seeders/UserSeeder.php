5115db7210714c4473dd551c4b8e0edaa0b2c7cf
mvdcreativo <mvdcreativo@gmail.com>
2021-05-17 02:04:44 -0300

    seeder user

--- database/seeders/UserSeeder.php
+++ database/seeders/UserSeeder.php
@@ -17,12 +17,21 @@ class UserSeeder extends Seeder
     {
 
         User::factory(5)->create();
-        User::create([
+        $userAdmin = User::create([
             'name'=>'Emir',
             'last_name'=>'Mendez',
             'email'=>'mvdcreativo@gmail.com',
             'password'=> Hash::make('12345678')
+        ]); 
+        $userAdmin->syncRoles([
+            'patient.index', 'patient.show', 'patient.create', 'patient.update','patient.delete',
+            'sample.index', 'sample.show', 'sample.create', 'sample.update','sample.delete',
+            'user.index', 'user.show', 'user.create', 'user.update','user.delete',
+            'ubication.index', 'ubication.show', 'ubication.create', 'ubication.update','ubication.delete',
+            'role.index', 'role.show', 'role.create', 'role.update','role.delete',
+            'permission.index', 'permission.show', 'permission.create', 'permission.update','permission.delete',
         ]);
+
 
 
     }
